6af1c429de822eeffcf63087dc77b598f0236410
Jerald George <jjjera>
2019-02-15 20:07:25 +0530

    wip

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import audioconcat from 'audioconcat';
 
 class App extends Component {
+
+  componentDidMount() {
+
+    console.log('componentDidMount called');
+
+      const songs =
+        ['everything_changed.mp3',
+        'fear_is_a_liar.mp3',
+        'my_liberty.mp3'];
+      console.log('songs are',songs);
+
+       audioconcat(songs)
+       .concat('all.mp3')
+       .on('start', function (command) {
+         console.log('ffmpeg process started:', command)
+       })
+       .on('error', function (err, stdout, stderr) {
+         console.error('Error:', err)
+         console.error('ffmpeg stderr:', stderr)
+       })
+       .on('end', function (output) {
+         console.error('Audio created in:', output)
+       })
+       console.log('audioconcat result is',audioconcat);
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          hai
         </header>
       </div>
     );
