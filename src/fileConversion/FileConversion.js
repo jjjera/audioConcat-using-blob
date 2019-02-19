bb1d7f88d77da970c9db03edbe58bfb683a848e4
Jerald George <jjjera>
2019-02-19 13:42:15 +0530

    wip

--- src/fileConversion/FileConversion.js
+++ src/fileConversion/FileConversion.js
@@ -3,40 +3,49 @@ import React, { Component } from 'react';
 class FileConversion extends Component {
 constructor(props) {
   super(props);
-  this.state = {encodedValue:''};
-  console.log('encodedValue is',this.state.encodedValue);
+  this.state = {mergedAudio:''};
+  // console.log('mergedAudio is',this.state.mergedAudio);
+  // this.getBase64Format = this.getBase64Format.bind(this);
 }
-
+// componentDidMount() {
+//   console.log('state value is',this.state.mergedAudio);
+// }
   goToEvaluate = () => {
-     console.log('func called');
+     console.log('goToEvaluate func called');
      document.getElementById('button').addEventListener('click', () => {
      var files = document.getElementById('file').files;
      if (files.length > 0) {
-     getBase64(files[0]);
-     // console.log('getBase64',getBase64);
+     this.getBase64Format(files[0]);
+     // console.log('getBase64Format',getBase64Format);
     }
     });
-     function getBase64(file) {
-     console.log('getBase64 called');
-     var reader = new FileReader();
-     reader.readAsDataURL(file);
-     // console.log('reader is',reader);
-     reader.onload = (state) => {
-     // console.log('result is',reader.result);
-     const endResult = reader.result;
-     console.log('endResult is',endResult);
-     let exValue = this.state.encodedValue + endResult;
-     console.log('exValue',exValue);
-     this.setState({encodedValue:exValue});
-   };
-    reader.onerror = function (error) {
-     console.log('Error: ', error);
-   };
- }
   }
 
+  getBase64Format = (file) => {
+     console.log('getBase64Format func called');
+     const reader = new FileReader();
+     reader.readAsDataURL(file);
+     // console.log('reader is',reader);
+         reader.onload = () => {
+          console.log('reader.onload func called');
+         // console.log('result is',reader.result);
+         const content = reader.result;
+         // console.log('content is',content);
+         // const endResult = this.state.mergedAudio + content;
+         // console.log('endResult',endResult);
+         const enc = window.btoa(this.state.mergedAudio + content);
+         const dec = window.atob(enc);
+         // console.log('dec is',dec);
+         this.setState({mergedAudio:dec});
+         console.log('setState is',this.state.mergedAudio);
+        };
+      reader.onerror =  (error) => {
+       console.log('Error: ', error);
+      };
+    }
+
   render() {
-    console.log('final res',this.state.encodedValue);
+    console.log('final res',this.state.mergedAudio);
     return (
       <div>
         <input id="file" type="file"/>
@@ -45,7 +54,7 @@ constructor(props) {
         <button id="button" onClick={this.goToEvaluate}>Evaluate</button>
           <br/>
           <br/>
-          <audio id="audio" src={this.state.encodedValue} controls autoPlay/>
+          <audio id="audio" src={this.state.mergedAudio} controls autoPlay />
       </div>
     );
   }
